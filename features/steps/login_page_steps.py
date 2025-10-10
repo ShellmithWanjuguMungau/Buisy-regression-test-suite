682efb5abc19bd095e3e955353c1f324783e40d0
Shellmith Wanjugu <testshelly29@gmail.com>
2025-10-10 10:23:26 +0300

    add waits and dashboard test scenario

--- features/steps/login_page_steps.py
+++ features/steps/login_page_steps.py
@@ -1,6 +1,7 @@
 from time import sleep
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from behave import given, when, then
 
@@ -20,21 +21,27 @@ def enter_correct_password(context):
 @when('click login button')
 def click_login(context):
     context.driver.find_element(By.CSS_SELECTOR, '[type="submit"]').click()
+    # sleep(3)
 
 
 @when("navigate to main login url")
 def navigate_to_main_page(context):
     # to bypass authentication
+    context.driver.wait.until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '[class="widget-main"]'))
+    )
     context.driver.get("https://dev.burnmfgsystems.com/")
-    sleep(3)
+    # sleep(3)
 
 @when("enter incorrect email")
 def enter_incorrect_email(context):
     context.driver.find_element(By.CSS_SELECTOR, '[name*="username"]').send_keys("qaaa.software@burnmfg.com")
 
+
 @when("enter incorrect password")
 def enter_correct_password(context):
     context.driver.find_element(By.CSS_SELECTOR, '[name*="password"]').send_keys("1233@Team")
+
 
 @when("Click from the dropdown {language}")
 def click_language(context,language):
@@ -42,27 +49,50 @@ def click_language(context,language):
     select=Select(dropdown)
     select.select_by_value(language)
 
+@when("click the stock and cash app tracker card")
+def click_the_stock_and_cash_app(context):
+    context.driver.wait.until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, '[href*="stock"]'))
+    ).click()
+
 
 @then("confirm welcome is displayed")
 def confirm_welcome_message(context):
-    actual_text=context.driver.find_element(By.XPATH, "//a[contains(text(),'Welcome')]").text
+    actual_text=context.driver.wait.until(
+        EC.presence_of_element_located((By.XPATH, "//a[contains(text(),'Welcome')]"))
+    ).text
     print(actual_text)
 
     expected_text='Welcome'
+    print(expected_text)
     assert expected_text in actual_text, f'error message: Expected tex:  {expected_text} not found in {actual_text}'
-    assert expected_text in actual_text, f'Expected text {expected_text} not found in {actual_text}'
+    # assert expected_text in actual_text, f'Expected text {expected_text} not found in {actual_text}'
 
 @then("Invalid credentials error message is displayed")
 def invalid_credentials_message(context):
-    actual_text=context.driver.find_element(By.XPATH, "//div[contains(text(),'Invalid credentials')]").text
+    actual_text=context.driver.wait.until(
+        EC.visibility_of_element_located((By.XPATH, "//div[contains(text(),'Invalid credentials')]"))
+    ).text
+    print(actual_text)
     expected_text='Invalid credentials'
     assert expected_text in actual_text, f'error message: Expected text {expected_text} not found in {actual_text}'
 
+
 @then ("confirm {welcome_message} is displayed")
 def confirm_welcome_message(context, welcome_message):
-    actual_text=context.find_element("//h4").text
+    actual_text=context.driver.wait.until(
+        EC.visibility_of_element_located((By.XPATH, "//h4"))
+    ).text
     expected_text=welcome_message
     assert expected_text in actual_text, f'expected text {expected_text} not found in {actual_text}'
+
+@then("Confirm that 9 cards are displayed")
+def confirm_9_cards_displayed(context):
+    dashboard_cards=context.driver.wait.until(
+        EC.visibility_of_all_elements_located((By.CSS_SELECTOR, '[class*="dashboard_tab"]'))
+    )
+    print(len(dashboard_cards))
+    assert len(dashboard_cards) == 9 , 'Excpected 9 cards displayed but got {}'.format(len(dashboard_cards))
 
 
 
--- features/tests/login.feature
+++ features/tests/login.feature
@@ -2,6 +2,7 @@
 Feature: Login feature
   confirm that that the login functionality and any other feature on the login page works as expected.
 
+
   Scenario: Verify user can login successfully with correct credentials
     Given User opens buisy login page
     When enter correct email
@@ -9,6 +10,8 @@ Feature: Login feature
     And click login button
     And navigate to main login url
     Then confirm welcome is displayed
+    When click the stock and cash app traccker card
+
 
   Scenario: Verify user cannot login with incorrect credentials
     Given User opens buisy login page
@@ -16,6 +19,7 @@ Feature: Login feature
     And enter incorrect password
     And click login button
     Then Invalid credentials error message is displayed
+
 
     Scenario Outline: Verify that changing language works
       Given User opens buisy login page
@@ -27,5 +31,15 @@ Feature: Login feature
         |SW       |Tafadhali Ingiza Habari yako     |
         |FR       |Veuillez saisir vos informations |
         |PT       |Insira suas informações          |
+
+    @single
+    Scenario: Verify that agent can access dashboard on login
+    Given User opens buisy login page
+    When enter correct email
+    And enter correct password
+    And click login button
+    And navigate to main login url
+    And click the stock and cash app tracker card
+    Then Confirm that 9 cards are displayed
 
 
