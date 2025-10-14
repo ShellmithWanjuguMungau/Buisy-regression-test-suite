8000ff9fd9d9c2d5c19131972e26fcb9a9571c21
Shellmith Wanjugu <testshelly29@gmail.com>
2025-10-14 06:50:15 +0300

    Add user-role test scenario

--- features/steps/User_Settings-user_roles_steps.py
+++ features/steps/User_Settings-user_roles_steps.py
@@ -18,6 +18,44 @@ def click_user_roles(context):
     ).click()
 
 
+@when("click create new user role")
+def click_create_new_user_role(context):
+    context.driver.wait.until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "#add-icon"))
+    ).click()
+
+
+@when("enter role name")
+def enter_role_name(context):
+    context.driver.wait.until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, "#field_role_name"))
+    ).send_keys('testrole')
+
+@when("check the permissions checkbox")
+def check_permissions_checkbox(context):
+    context.driver.wait.until(
+        EC.visibility_of_element_located(By.CSS_SELECTOR, '[class*="checkbox"]')
+    ).click()
+
+@when("click add user role")
+def add_user_role(context):
+    context.driver.find_element_by_css_selector("#btn-add").click()
+
+
+@when("click okay on the pop-up displayed")
+def click_okay_on_popup(context):
+    context.driver.wait.until(
+        EC.visibility_of_element_located((By.XPATH,"//button[text()='OK']"))
+    ).click()
+
+
+@when("enter TSM role on search")
+def enter_TSM_role(context):
+    context.driver.wait.until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, '[type="search"]'))
+    ).send_keys('TSM')
+
+
 @Then("verify {title} is displayed")
 def verify_user_settings(context, title):
     actual_text=context.driver.wait.until(
@@ -26,6 +64,7 @@ def verify_user_settings(context, title):
     print(actual_text)
     title="User roles"
     assert actual_text in title,f'{actual_text} is not found in {title}'
+
 
 @then("verify 13 records are displayed by footer info")
 def verify_user_roles(context):
@@ -39,3 +78,13 @@ def verify_user_roles(context):
     ).text
     Expected_text="Showing 1 to 13 "
     assert Expected_text in actual_text,f'{actual_text} is not found in {Expected_text}'
+
+
+@then ("verify TSM role is diplayed")
+def verify_TSM_role(context):
+    actual_text=context.driver.wait.until(
+        EC.visibility_of_element_located((By.XPATH,"//td[text()='TSM']"))
+    ).text
+    expected_text="TSM"
+    assert actual_text in expected_text,f'{actual_text} is not found in {expected_text}'
+
--- features/tests/User_Settings-user_roles.feature
+++ features/tests/User_Settings-user_roles.feature
@@ -2,7 +2,7 @@
 Feature: User Settings-user role feature
   verify user can view,add,edit,delete a user role
 
-  @single
+
   Scenario: verify user roles page opens and roles are displayed.
     Given User opens buisy login page
     When enter correct email
@@ -15,7 +15,32 @@ Feature: User Settings-user role feature
     Then verify title is displayed
     Then verify 13 records are displayed by footer info
 
-
-
-
+#  Scenario: Verify agent can create, edit, and delete a user role. #scenario does not work as expected .
+#  Given User opens buisy login page
+#    When enter correct email
+#    And enter correct password
+#    And click login button
+#    And navigate to main login url
+#    And click the stock and cash app tracker card
+#    And click user settings
+#    And click user roles
+#    And click create new user role
+#    And enter role name
+#    And check the permissions checkbox
+#    And click add user role
+#    And click okay on the pop-up displayed
+#    Then Verify success message on the alert
     # Enter steps here
+
+  @single
+  Scenario: Verify the search works
+    Given User opens buisy login page
+    When enter correct email
+    And enter correct password
+    And click login button
+    And navigate to main login url
+    And click the stock and cash app tracker card
+    And click user settings
+    And click user roles
+    And enter TSM role on search
+    Then verify TSM role is diplayed
